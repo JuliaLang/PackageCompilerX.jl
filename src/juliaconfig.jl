073262cb06476477e1cf01a9601614eba775cec9
Kristoffer Carlsson <kcarlsson89@gmail.com>
2019-11-29 21:45:55 +0100

    rpath-link is the key

--- src/juliaconfig.jl
+++ src/juliaconfig.jl
@@ -42,10 +42,7 @@ function ldlibs(relative_path=nothing)
         "julia"
     end
     if Sys.isunix()
-        # TODO, these should not be needed since it adds an rpath to the current julia process libraries.
-        # return "-Wl,-rpath,$(shell_escape(julia_libdir())) -l$libname"
-        return "-L$(shell_escape(julia_libdir())) -L$(shell_escape(julia_private_libdir())) -l$libname"
-        #return "-l$libname"
+        return "-Wl,-rpath-link,$(shell_escape(julia_libdir())) -Wl,-rpath-link,$(shell_escape(julia_private_libdir())) -l$libname"
     else
         return "-l$libname -lopenlibm"
     end
