61739b8004c41cdce20a57a1f889db1d9981b413
KristofferC <kcarlsson89@gmail.com>
2019-12-09 18:31:18 +0100

    fdsf

--- src/hacks.jl
+++ src/hacks.jl
@@ -8,8 +8,10 @@ function ugly_workarounds()
     hack_code *= """
     @eval Module() begin
         plots_id = Base.PkgId(Base.UUID("91a5bcdd-55d7-5caf-9e0b-520d859cae80"), "Plots")
+        @show plots_id
         if haskey(Base.loaded_modules, plots_id)
             gr_id = Base.PkgId(Base.UUID("28b8d3ca-fb5f-59d9-8090-bfdbd6d07a71"), "GR")
+            @show gr_id
             GR = get(Base.loaded_modules, gr_id, nothing)
             if GR !== nothing
                 GR.__init__()
